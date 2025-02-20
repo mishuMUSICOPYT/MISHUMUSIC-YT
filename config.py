80d30f679602cda034dc0ba30e9179e22a34372e
CYBER-YT <anilkumarmishraji52@gmail.com>
2025-02-21 05:01:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RAJA-BABY/VIPMUSIC",
+    "https://github.com/CYBER-YT/NYSAMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
