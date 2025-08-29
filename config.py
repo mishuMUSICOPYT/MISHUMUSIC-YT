6471e12368a2b1a8e5908c7a1b0e343e97915df7
mishuMUSICOPYT <mishumusicyt@gmail.com>
2025-08-29 13:51:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/NIKKI-YT/MISHUMUSIC-YT",
+    "https://github.com/mishuMUSICOPYT/MISHUMUSIC-YT",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
@@ -117,6 +117,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
