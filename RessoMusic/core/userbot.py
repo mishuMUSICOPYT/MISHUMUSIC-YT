87cbce64cd37a8f430ddf54a358eae7f02fd2bb4
mishuMUSICOPYT <mishumusicyt@gmail.com>
2025-09-25 19:57:08 +0530

    Update userbot.py

--- RessoMusic/core/userbot.py
+++ RessoMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("StylishNamesFont")
-                await self.one.join_chat("BOTXPRO1")
+                await self.one.join_chat("its_love_xd")
+                await self.one.join_chat("its_love_xd")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("StylishNamesFont")
-                await self.one.join_chat("BOTXPRO1")
+                await self.two.join_chat("ots_love_xd")
+                await self.one.join_chat("its_love_xd")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("StylishNamesFont")
-                await self.one.join_chat("BOTXPRO1")
+                await self.three.join_chat("its_love_xd")
+                await self.one.join_chat("its_love_xd")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("StylishNamesFont")
-                await self.one.join_chat("BOTXPRO1")
+                await self.four.join_chat("its_love_xd")
+                await self.one.join_chat("its_love_xd")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("StylishNamesFont")
-                await self.one.join_chat("BOTXPRO1")
+                await self.five.join_chat("its_love_xd")
+                await self.one.join_chat("its_love_xd")
             except:
                 pass
             assistants.append(5)
@@ -168,3 +168,4 @@ class Userbot(Client):
                 await self.five.stop()
         except:
             pass
+
