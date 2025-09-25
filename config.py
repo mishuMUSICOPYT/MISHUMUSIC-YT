b18296da87e9a55776ad391f52c412d9b56d843f
mishuMUSICOPYT <mishumusicyt@gmail.com>
2025-09-25 23:34:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/its_Love_xd")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+21nlZZAaT_czMTk9")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/indian_chatting00")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -117,6 +117,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
